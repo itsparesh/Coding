89cb1b682df9a391a769bfab76a7f1f6f3c51693
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-06-17 21:36:43 +0100

    2033-THE-NUMBER-OF-FULL-ROUNDS-YOU-HAVE-PLAYED Stats: Time: 0 ms (100%), Space: 41.8 MB (31.65%) - LeetHub

--- 2033-the-number-of-full-rounds-you-have-played/2033-the-number-of-full-rounds-you-have-played.java
+++ 2033-the-number-of-full-rounds-you-have-played/2033-the-number-of-full-rounds-you-have-played.java
@@ -1,20 +1,20 @@
 class Solution {
     public int numberOfRounds(String loginTime, String logoutTime) {
-        int inHour = Integer.parseInt(loginTime.substring(0,2));
-        int inMin = Integer.parseInt(loginTime.substring(3));
-        int outHour = Integer.parseInt(logoutTime.substring(0,2));
-        int outMin = Integer.parseInt(logoutTime.substring(3));
+        int inMin = timeToMinutes(loginTime);
+        int outMin = timeToMinutes(logoutTime);
 
-        if (outHour > inHour || (outHour == inHour && outMin >= inMin)) {
-            outMin = outHour * 60 + outMin;
-            inMin = inHour * 60 + inMin;
-        } else {
-            inMin = inHour * 60 + inMin;
-            outMin = outHour * 60 + outMin + 24 * 60;
+        if (outMin < inMin) {
+            outMin += 24 * 60;
         }
 
         inMin = ((inMin + 14) / 15) * 15;
         outMin = (outMin / 15) * 15;
         return Math.max(0, (outMin - inMin) / 15);
+    }
+
+    private int timeToMinutes(String time) {
+        int h = Integer.parseInt(time.substring(0, 2));
+        int m = Integer.parseInt(time.substring(3));
+        return h * 60 + m;
     }
 }
