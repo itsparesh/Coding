45a7bf6c82f671f00860d0c5d119da1d517d7e58
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-10 23:18:25 +0100

    0221-MAXIMAL-SQUARE Stats: Time: 8 ms (69.59%), Space: 60.7 MB (16.96%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -5,18 +5,18 @@ class Solution {
         if (m == 0 || n == 0) {
             return 0;
         }
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
         int max = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
+                if(matrix[i][j] == '1') {
                     if (i == 0 || j == 0) {
-                        memo[i][j] = 1;
+                        dp[i][j] = 1;
                     } else {
-                        memo[i][j] = Math.min(Math.min(memo[i-1][j], memo[i][j-1]), memo[i-1][j-1]) + 1;
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
                     }
-                    max = Math.max(max, memo[i][j]);
                 }
+                max = Math.max(max, dp[i][j]);
             }
         }
         return max * max;
