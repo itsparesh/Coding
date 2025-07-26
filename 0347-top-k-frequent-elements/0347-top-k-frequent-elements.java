eece0500080e9d1243efdc61821f4c1e22e416ee
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-26 13:29:08 +0100

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 13 ms (76.48%), Space: 48.4 MB (73.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,20 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         HashMap<Integer, Integer> countMap = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+        // 1. Count frequencies
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
-        maxHeap.addAll(countMap.entrySet());
-
-        int[] res = new int[k];
-
-        for (int i = 0; i < k; i++) {
-            res[i] = maxHeap.poll().getKey();
+        // 2. Build bucket: index = frequency, the value is the list of numbers with that frequency
+        List<Integer>[] countList = new ArrayList[nums.length + 1];
+        for (int key : countMap.keySet()) {
+            int count = countMap.get(key);
+            if (countList[count] == null) {
+                countList[count] = new ArrayList<Integer>();
+            }
+            countList[count].add(key);
         }
 
-        return res;
+        // 3. Collect k most frequent from the buckets, going from high to low frequency
+        List<Integer> res = new ArrayList<>();
+        for (int i = nums.length; i >= 0 && res.size() < k; i--) {
+            if (countList[i] != null) {
+                res.addAll(countList[i]);
+            }
+        }
+
+        // 4. Return only k elements
+        return res.stream().limit(k).mapToInt(Integer::intValue).toArray();
     }
 }
