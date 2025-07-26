c8763bc5c333e23c37a6bb4667fac55b98677ad5
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-26 13:30:24 +0100

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 11 ms (91.72%), Space: 45.4 MB (97.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> countMap = new HashMap<>();
-        // 1. Count frequencies
         for (int num : nums) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
-        // 2. Build bucket: index = frequency, the value is the list of numbers with that frequency
         List<Integer>[] countList = new ArrayList[nums.length + 1];
         for (int key : countMap.keySet()) {
             int count = countMap.get(key);
@@ -16,15 +14,17 @@ class Solution {
             countList[count].add(key);
         }
 
-        // 3. Collect k most frequent from the buckets, going from high to low frequency
-        List<Integer> res = new ArrayList<>();
-        for (int i = nums.length; i >= 0 && res.size() < k; i--) {
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = nums.length; i >= 0 && index < k; i--) {
             if (countList[i] != null) {
-                res.addAll(countList[i]);
+                for (int num : countList[i]) {
+                    res[index++] = num;
+                    if (index == k) break;
+                }
             }
         }
 
-        // 4. Return only k elements
-        return res.stream().limit(k).mapToInt(Integer::intValue).toArray();
+        return res;
     }
 }
