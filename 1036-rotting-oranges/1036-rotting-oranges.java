8229999bd440fbc8d04da5b869333d1b4746c597
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-28 14:13:42 +0100

    1036-ROTTING-ORANGES Stats: Time: 2 ms (67.68%), Space: 42.5 MB (49.66%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -6,11 +6,11 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         ROW = grid.length;
         COL = grid[0].length;
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
-                    queue.add(new Pair<Integer, Integer>(i, j));
+                    queue.add(new int[]{i, j});
                 } 
                 if (grid[i][j] == 1) {
                     fresh++;
@@ -22,22 +22,20 @@ class Solution {
         return bfs(grid, queue);
     } 
 
-    private int bfs(int[][] grid, Queue<Pair<Integer, Integer>> queue) {
+    private int bfs(int[][] grid, Queue<int[]> queue) {
         int minutes = 0;
         
         while (!queue.isEmpty() && fresh > 0) {
             int size = queue.size();
-            boolean rotten = false;
             for (int j = 0; j < size; j++) {
-                Pair<Integer, Integer> pair = queue.poll();
+                int[] pair = queue.poll();
                 for (int[] direction: DIRECTIONS) {
-                    int nrow = pair.getKey() + direction[0];
-                    int ncol = pair.getValue() + direction[1];
+                    int nrow = pair[0] + direction[0];
+                    int ncol = pair[1] + direction[1];
                     if (nrow >= 0 && nrow < ROW && ncol >= 0 && ncol < COL && grid[nrow][ncol] == 1) {
                         grid[nrow][ncol] = 2;
-                        queue.add(new Pair<Integer, Integer>(nrow, ncol));
+                        queue.add(new int[]{nrow, ncol});
                         fresh--;
-                        rotten = true;
                     }
                 }
             }
