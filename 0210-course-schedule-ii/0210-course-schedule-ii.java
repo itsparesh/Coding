3be79c3c084d8071c2b4f866589b0d6f9a55622f
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-30 13:21:12 +0100

    0210-COURSE-SCHEDULE-II Stats: Time: 8 ms (15.72%), Space: 46.3 MB (20.64%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -11,17 +11,17 @@ class Solution {
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(prerequisites, i)) return new int[0];
+            if (!dfs(i)) return new int[0];
         }
         return output.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    private boolean dfs(int[][] prerequisites, int node) {
+    private boolean dfs(int node) {
         if (cycle.contains(node)) return false;
         if (visited.contains(node)) return true;
         cycle.add(node);
         for(int num: adjacencyMap.getOrDefault(node, new ArrayList<>())) {
-            if (!dfs(prerequisites, num)) return false;
+            if (!dfs(num)) return false;
         }
         cycle.remove(node);
         visited.add(node);
