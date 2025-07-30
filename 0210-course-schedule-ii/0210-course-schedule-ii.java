839d238e4597b82a6e2efd177d24e254bb1f65bf
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-07-30 13:27:21 +0100

    0210-COURSE-SCHEDULE-II Stats: Time: 7 ms (27.68%), Space: 46.1 MB (54.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -3,29 +3,33 @@ class Solution {
     HashMap<Integer, List<Integer>> adjacencyMap = new HashMap<>();
     Set<Integer> visited = new HashSet<>();
     Set<Integer> cycle = new HashSet<>();
-    ArrayList<Integer> output = new ArrayList<>();
+    int[] output;       // array to store the result
+    int index;          // current index to insert into output (starting from end)
 
     public int[] findOrder(int numCourses, int[][] prerequisites) {  
+        output = new int[numCourses];
+        index = numCourses - 1;
         for(int[] arr: prerequisites) {
             adjacencyMap.computeIfAbsent(arr[0], k -> new ArrayList<>()).add(arr[1]);
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(i)) return new int[0];
+            if (!dfs(numCourses, i)) return new int[0];
         }
-        return output.stream().mapToInt(Integer::intValue).toArray();
+        return output;
     }
 
-    private boolean dfs(int node) {
+    private boolean dfs(int numCourses, int node) {
         if (cycle.contains(node)) return false;
         if (visited.contains(node)) return true;
         cycle.add(node);
         for(int num: adjacencyMap.getOrDefault(node, new ArrayList<>())) {
-            if (!dfs(num)) return false;
+            if (!dfs(numCourses, num)) return false;
         }
         cycle.remove(node);
         visited.add(node);
-        output.add(node);
+        output[(numCourses - 1) - index] = node;
+        index--;
         return true;
     }
 }
