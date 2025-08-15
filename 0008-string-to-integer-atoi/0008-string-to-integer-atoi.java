3bea4700c914e80510c9b167a474467dd7389192
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-15 18:44:19 +0100

    0008-STRING-TO-INTEGER-ATOI Stats: Time: 1 ms (100%), Space: 42.6 MB (47.13%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,8 +3,7 @@ class Solution {
         int res = 0;
         s = s.trim();
         boolean isNegative = false;
-        int len = s.length();
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (i == 0) {
                 if (ch == '-' && !isNegative) {
@@ -14,7 +13,6 @@ class Solution {
                 else if (ch == '+') {
                     continue;
                 }
-                
             }
             if (!Character.isDigit(ch)) {
                 break;
@@ -23,7 +21,7 @@ class Solution {
             if (res > (Integer.MAX_VALUE - num) / 10) {
                 return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
-            res = num + res * 10;
+            res = res * 10 + num;
         }
         return isNegative? res * -1: res;
     }
