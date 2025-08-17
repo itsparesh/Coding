97bf17fe4e17b4d07fc1a3b69bd13bd60d7f50ae
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-17 22:01:35 +0100

    0013-ROMAN-TO-INTEGER Stats: Time: 2 ms (100%), Space: 44.7 MB (83.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,24 +1,77 @@
 class Solution {
     public int romanToInt(String s) {
-        int[] values = new int[128];
-        values['I'] = 1;
-        values['V'] = 5;
-        values['X'] = 10;
-        values['L'] = 50;
-        values['C'] = 100;
-        values['D'] = 500;
-        values['M'] = 1000;
-
         int res = 0;
-        int len = s.length();
-        for (int i = len - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            if (i + 1 < len && values[ch] < values[s.charAt(i + 1)]) {
-                res -= values[ch];
-            } else {
-                res += values[ch];
+        int i = s.length() - 1;
+
+        while (i >= 0) {
+            switch (s.charAt(i)) {
+                case 'I':
+                    res += 1;
+                    i--;
+                    break;
+
+                case 'V':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'I') {
+                        res += 4;
+                        i -= 2;
+                    } else {
+                        res += 5;
+                        i--;
+                    }
+                    break;
+
+                case 'X':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'I') {
+                        res += 9;
+                        i -= 2;
+                    } else {
+                        res += 10;
+                        i--;
+                    }
+                    break;
+
+                case 'L':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'X') {
+                        res += 40;
+                        i -= 2;
+                    } else {
+                        res += 50;
+                        i--;
+                    }
+                    break;
+
+                case 'C':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'X') {
+                        res += 90;
+                        i -= 2;
+                    } else {
+                        res += 100;
+                        i--;
+                    }
+                    break;
+
+                case 'D':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'C') {
+                        res += 400;
+                        i -= 2;
+                    } else {
+                        res += 500;
+                        i--;
+                    }
+                    break;
+
+                case 'M':
+                    if (i - 1 >= 0 && s.charAt(i - 1) == 'C') {
+                        res += 900;
+                        i -= 2;
+                    } else {
+                        res += 1000;
+                        i--;
+                    }
+                    break;
             }
         }
+
         return res;
     }
 }
