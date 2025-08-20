937e3674b31f3cc1f84031551e84131c9ebd6568
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-20 13:43:05 +0100

    0015-3SUM Stats: Time: 30 ms (70.1%), Space: 51.5 MB (69.87%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,26 +3,24 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         int len = nums.length;
-
         for (int i = 0; i < len - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                // Skip duplicate values for i
-                continue;
-            }
-            int j = i + 1, k = len - 1;
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = len - 1;
             while (j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    // Skip duplicates for j and k after adding the triplet
-                    while (j < k && nums[j] == nums[j + 1]) j++;
-                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));   
+                    while (j < k && nums[j] == nums[j+1]) j++;
+                    while (j < k && nums[k] == nums[k-1]) k--;                 
                     j++;
                     k--;
-                } else if (sum < 0) {
-                    j++;
                 } else {
-                    k--;
+                    if (sum < 0) {
+                        j++;
+                    } else {
+                        k--;
+                    }
                 }
             }
         }
