36848cd16ea68f3c2749a460aa1f12454ca28f40
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-20 13:35:49 +0100

    0015-3SUM Stats: Time: 31 ms (56.56%), Space: 51.7 MB (47.62%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        Set<String> checkSet = new HashSet<>();
         Arrays.sort(nums);
         int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            int x = nums[i];
-            int j = i + 1;
-            int k = len - 1;
+
+        for (int i = 0; i < len - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // Skip duplicate values for i
+                continue;
+            }
+            int j = i + 1, k = len - 1;
             while (j < k) {
-                int y = nums[j];
-                int z = nums[k];
-                int sum = x + y + z;
+                int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
-                    String checkString = x + "" + y + ""+z;
-                    if (!checkSet.contains(checkString)) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(x);
-                        list.add(y);
-                        list.add(z);
-                        res.add(list);
-                        checkSet.add(checkString);
-                    }
-                    
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    // Skip duplicates for j and k after adding the triplet
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
                     j++;
                     k--;
+                } else if (sum < 0) {
+                    j++;
                 } else {
-                    if (sum < 0) {
-                        j++;
-                    } else {
-                        k--;
-                    }
+                    k--;
                 }
             }
         }
