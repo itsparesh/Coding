411f0ed61f867fbd58724c729b9958d266ecd13f
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-24 15:28:00 +0100

    0020-VALID-PARENTHESES Stats: Time: 3 ms (37.77%), Space: 42.1 MB (19.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch == ')') {
