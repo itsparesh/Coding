f8c6f0d74c8dda17a243e5ea0c8e9f5dfa24a3df
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-08-24 15:29:13 +0100

    0020-VALID-PARENTHESES Stats: Time: 3 ms (37.77%), Space: 41.8 MB (66.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,29 +3,29 @@ class Solution {
         ArrayDeque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch == ')') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    return false;
+            switch (ch) {
+                case ')' -> {
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
                 }
-            }
-            else if (ch == '}') {
-                if (!stack.isEmpty() && stack.peek() == '{') {
-                    stack.pop();
-                } else {
-                    return false;
+                case '}' -> {
+                    if (!stack.isEmpty() && stack.peek() == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
                 }
-            }
-            else if (ch == ']') {
-                if (!stack.isEmpty() && stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    return false;
+                case ']' -> {
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
                 }
-            } 
-            else {
-                stack.push(ch);
+                default -> stack.push(ch);
             }
         }
         return stack.size() == 0;
