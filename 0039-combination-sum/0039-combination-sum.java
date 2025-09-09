06682d0e2804bb69d9a6d8c503dce3b78fc75143
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-09-09 14:20:25 +0100

    0039-COMBINATION-SUM Stats: Time: 2 ms (83.84%), Space: 44.8 MB (31.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,17 +1,19 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
+    private List<List<Integer>> result = new ArrayList<>();
+    private int len;
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        len = candidates.length;
         findCombination(candidates, target, 0, new ArrayList<>(), 0);
         return result;
     }
-
     private void findCombination(int[] candidates, int target, int index, ArrayList<Integer> current, int total) {
         if (total == target) {
             result.add(new ArrayList<>(current));
             return;
         }
 
-        if (index >= candidates.length || total > target) {
+        if (index >= len || total > target) {
             return;
         }
 
