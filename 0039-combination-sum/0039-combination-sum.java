5346f671132fd6ea78a022ca50fb6fd60ea40dcb
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-09-09 14:46:55 +0100

    0039-COMBINATION-SUM Stats: Time: 2 ms (83.84%), Space: 44.8 MB (31.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,7 +2,6 @@ class Solution {
     private List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         findCombination(candidates, target, 0, new ArrayList<>(), 0);
         return result;
     }
@@ -12,17 +11,13 @@ class Solution {
             return;
         }
 
-        if (total > target) {
+        if (index >= candidates.length || total > target) {
             return;
         }
 
-        for (int i = index; i < candidates.length; i++) {
-            if (total + candidates[i] > target) {
-                break;
-            }
-            current.add(candidates[i]);
-            findCombination(candidates, target, i, current, total + candidates[i]);
-            current.removeLast();
-        }
+        current.add(candidates[index]);
+        findCombination(candidates, target, index, current, total + candidates[index]);
+        current.remove(current.size() - 1);
+        findCombination(candidates, target, index + 1, current, total);
     }
 }
