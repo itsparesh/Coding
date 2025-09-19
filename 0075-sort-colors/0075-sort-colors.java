d4ce8c2a204aee972194e509f86d83a1835f6aa6
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-09-19 16:15:14 +0100

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 41.7 MB (97.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zeros = 0, ones = 0, twos = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeros++;
-            } else if (nums[i] == 1) {
-                ones++;
+        int current = 0, first = 0, last = nums.length - 1;
+        while (current <= last) {
+            if (nums[current] == 0) {
+                int temp = nums[current];
+                nums[current] = nums[first];
+                nums[first] = temp;
+                current++;
+                first++;
+            } else if (nums[current] == 1) {
+                current++;
             } else {
-                twos++;
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (zeros > 0) {
-                nums[i] = 0;
-                zeros--;
-            } else if (ones > 0) {
-                nums[i] = 1;
-                ones--;
-            } else {
-                nums[i] = 2;
-                twos--;
+                int temp = nums[current];
+                nums[current] = nums[last];
+                nums[last] = temp;
+                last--;
             }
         }
     }
