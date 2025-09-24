ba31b4e1ac194ebae821c982815f63771edc2ac5
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-09-24 10:10:46 +0100

    2005-CHECK-IF-ALL-THE-INTEGERS-IN-A-RANGE-ARE-COVERED Stats: Time: 0 ms (100%), Space: 42.3 MB (11.11%) - LeetHub

--- 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.java
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.java
@@ -1,30 +1,23 @@
 class Solution {
     public boolean isCovered(int[][] ranges, int left, int right) {
-        //Arrays.sort(ranges, (a, b) -> Integer.compare(a[0], b[0]));
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int[] diff = new int[53];
         for (int[] arr: ranges) {
-            if (map.containsKey(arr[0])) {
-                map.put(arr[0], Math.max(map.get(arr[0]), arr[1]));
-            } else {
-                map.put(arr[0], arr[1]);
-            }  
+            diff[arr[0]] += 1;
+            if (arr[1]+ 1  < diff.length) {
+                diff[arr[1]+ 1] -= 1;
+            }
         }
 
-        boolean result = true;
+        for (int i = 1; i < diff.length; i++) {
+            diff[i] += diff[i-1];
+        }
 
         for (int i = left; i <= right; i++) {
-            result = false;
-            for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-                if (entry.getKey() <= i && entry.getValue() >= i) {
-                    result = true;
-                    break;
-                }
-            }
-            if (!result) {
+            if (diff[i] <= 0) {
                 return false;
             }
         }
         
-        return result;
+        return true;
     }
 }
