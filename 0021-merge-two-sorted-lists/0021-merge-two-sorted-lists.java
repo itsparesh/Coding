80d7b3799c932f6a4caa1e74a10058e3ce61f70f
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-10-01 12:19:26 +0100

    0021-MERGE-TWO-SORTED-LISTS Stats: Time: 0 ms (100%), Space: 42.7 MB (34.9%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -16,7 +16,7 @@ class Solution {
             if (list1.val <= list2.val) {
                 node.next = list1;
                 list1 = list1.next;
-            } else if (list1.val > list2.val) {
+            } else {
                 node.next = list2;
                 list2 = list2.next;
             } 
