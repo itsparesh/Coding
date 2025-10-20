364cf35a5be094aae9f397996d77dbe7793351c4
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-10-20 21:45:27 +0100

    0053-MAXIMUM-SUBARRAY Stats: Time: 1 ms (99.39%), Space: 57.5 MB (13.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         if (nums.length == 1) return nums[0];
-        int maxOverall = nums[0], maxEnding = nums[0];
+        int maxSum = nums[0], currentSum = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            maxEnding = Math.max(nums[i], maxEnding + nums[i]); 
-            maxOverall = Math.max(maxOverall, maxEnding);
+            currentSum = Math.max(nums[i], currentSum + nums[i]); 
+            maxSum = Math.max(maxSum, currentSum);
         }
-        return maxOverall;
+        return maxSum;
     }
 }
