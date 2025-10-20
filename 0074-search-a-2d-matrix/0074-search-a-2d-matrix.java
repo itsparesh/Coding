e5789ba5e869eec4ac9386ece0d1c7d07816b425
Paresh Krishna Sharma <master.pareshkrishna@gmail.com>
2025-10-20 12:45:32 +0100

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 42.6 MB (18.68%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -6,15 +6,14 @@ class Solution {
             int mid = left + (right - left) / 2;
             int row = mid / n;
             int col = mid % n;
-            
             if (matrix[row][col] == target) {
                 return true;
-            } else if (matrix[row][col] < target) {
+            } else if (matrix[row][col] <= target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
-        }  
-        return false; 
+        }
+        return false;
     }
 }
